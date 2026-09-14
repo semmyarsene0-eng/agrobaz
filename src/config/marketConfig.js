280d9ce87703b489ee1fb70470d468dcc80a8c6a
semmyarsene0-eng <semmyarsene0@gmail.com>
2026-09-14 10:04:02 +0300

    Create marketConfig.js

---
+++ src/config/marketConfig.js
@@ -0,0 +1,27 @@
+const marketConfig = {
+  defaultCountry: "UG",
+
+  supportedCountries: [
+    "UG",
+    "KE",
+    "TZ",
+    "RW",
+    "NG",
+    "GH",
+    "ZA",
+  ],
+
+  defaultCurrency: "UGX",
+
+  defaultLanguage: "en",
+
+  marketFeatures: {
+    localPrices: true,
+    globalPrices: true,
+    currencyConversion: true,
+    countrySelection: true,
+    languageSelection: true,
+  },
+};
+
+export default marketConfig;
